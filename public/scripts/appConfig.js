d76e2705cea2ae837db94f802b1a1e2c593aae08
Andrew Dourgarian <andrewdourgarian@yahoo.com>
2016-04-01 01:57:16 -0500

    filling out content, formatting

--- public/scripts/appConfig.js
+++ public/scripts/appConfig.js
@@ -73,6 +73,40 @@ app.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout){
     }
   ];
 
+  $scope.text = {
+    bio : {
+      P1: "I am a full-stack web developer with hands on experience working alongside other developers and" +
+      " with clients to conceptualize, design, and build the most effective and convenient web applications for clients" +
+      " utilizing different plugins and web API’s for added utility. ",
+      P2: "I’ve learned several programming languages and best practices as a web developer, and I am striving" +
+      " to continue honing my current technologies and expand my skill set to become a stronger and more diverse software" +
+      " developer. Since my recent graduation from Prime Digital Academy, I’ve already learned Android development with" +
+      " Java and XML, two languages that I hadn’t had any experience with before, along with looking into open source" +
+      " cryptographic currency projects and developing in meteor.",
+      P3: "I love creating and I am always working on one project or another on my own time, whether it be" +
+      " music, art, a game, or learning a new skill. I am looking for a fast-paced and exciting work environment with an" +
+      " open, close-knit, and fun community. My number one priority is having the opportunity to work with new" +
+      " technologies and learn as much as possible."
+    },
+    resume : {
+      myBlog: {
+        listItems : [
+          "Designed and developed a social networking site for bloggers.",
+          "After registering as a user, a personal profile page is created. The user may upload a profile" +
+          " Picture(stored in the cloud at Cloudinary) and personal interests and a bio can be added. A personal blog is" +
+          " also created on that page where the user can post and other users can comment. By searching for other users" +
+          " in the search bar, the user can navigate to other users’ profile pages and subscribe to other users’ blogs." +
+          " All posts and comments on blogs the user is subscribed to are fed into the newsfeed page.",
+          "Wrote a project scope to plan out the development process, project milestones, and goals.",
+          "Technologies used: MEAN Stack(MongoDB, Express, AngularJs, Node.js), Javascript, Cloud Storage API," +
+          " JS Web Tokens, Jade Templates, AJAX, CSS 3, Bootstrap, HTML 5.",
+          "Github Repository: https://github.com/ADourgarian/social_networking_site"
+        ]
+      }
+    }
+
+  };
+
   $scope.nav = function(css){
     $scope.currentPanel = css;
     $scope.loading = true;
